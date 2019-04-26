75f00a341eb0222a6eb7bb0a656308e2a3cfc71d
victor-domingos <victordomingos@gmail.com>
2019-04-26 14:23:04 -0300

    Removing unused imports

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -5,8 +5,6 @@ namespace AppBundle\Controller;
 use AppBundle\Service\RaceService;
 use AppBundle\Service\RacingHorseService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends Controller
